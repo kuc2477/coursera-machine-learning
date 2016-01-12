93286a5f92153ae542df6c131aa5c0823e253a56
June <kuc2477@gmail.com>
2016-01-13 03:56:19 +0900

    ex2: implement logistic function gradient and cost function J

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,12 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h = sigmoid(X * theta)
+e = h - y
 
-
-
-
-
-
+J = sum((-y) .* log(h) - (1-y) .* log(1 - h))
+grad = (1/m) * (e' * X)'
 
 
 % =============================================================
